d259749ed0ff5e9dea8898cc0a892a3745c80bcc
Frank Molenaar <info@frankmolenaar.net>
2024-08-15 10:34:17 +0200

    adds proper methods usable by react > 0.70

--- index.js
+++ index.js
@@ -31,4 +31,39 @@ HoneywellScanner.off = (eventName, handler) => {
   DeviceEventEmitter.removeListener(eventName, handler);
 };
 
+/**
+ * Listen for available events
+ * @param  {String} eventName Name of event one of barcodeReadSuccess, barcodeReadFail
+ * @param  {Function} handler Event handler
+ */
+
+const barcodeReaderEmitter = new NativeEventEmitter(HoneywellScanner);
+
+var subscriptionBarcodeReadSuccess = null;
+var subscriptionBarcodeReadFail = null;
+
+HoneywellScanner.onBarcodeReadSuccess = (handler) => {
+  subscriptionBarcodeReadSuccess?.remove();
+  subscriptionBarcodeReadSuccess = null;
+  subscriptionBarcodeReadSuccess = barcodeReaderEmitter.addListener(HoneywellScanner.BARCODE_READ_SUCCESS, handler);
+}
+
+HoneywellScanner.onBarcodeReadFail = (handler) => {
+  subscriptionBarcodeReadFail?.remove();
+  subscriptionBarcodeReadFail = null;
+  subscriptionBarcodeReadFail = barcodeReaderEmitter.addListener(HoneywellScanner.BARCODE_READ_FAIL, handler);
+}
+
+/**
+ * Stop listening for event
+ * @param  {String} eventName Name of event one of barcodeReadSuccess, barcodeReadFail
+ * @param  {Function} handler Event handler
+ */
+HoneywellScanner.offBarcodeReadSuccess = () => {
+  subscriptionBarcodeReadSuccess?.remove();
+};
+HoneywellScanner.offBarcodeReadFail = () => {
+  subscriptionBarcodeReadFail?.remove();
+};
+
 module.exports = HoneywellScanner;
